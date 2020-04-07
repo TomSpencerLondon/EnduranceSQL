d2e08a641de5c8a7473469aa5e53ee6c239b6b3a
Alexis Pavlidis <apavlidi@it.teithe.gr>
2020-04-07 20:34:47 +0300

    finish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@SpringBootApplication
 public class Main {
 
   private static Connection conn1;
 
   public static void main(String[] args) throws SQLException {
-    String url1 = "jdbc:mysql://localhost:3306/endurance";
-    String user = "root";
-    String password = "Buster";
-
-    conn1 = DriverManager.getConnection(url1, user, password);
-    if (conn1 != null) {
-      System.out.println("Connected to the database test1");
-    }
+    SpringApplication.run(Main.class, args);
+//    String url1 = "jdbc:mysql://localhost:3306/endurance";
+//    String user = "root";
+//    String password = "Buster";
+//
+//    conn1 = DriverManager.getConnection(url1, user, password);
+//    if (conn1 != null) {
+//      System.out.println("Connected to the database test1");
+//    }
 
 
   }
